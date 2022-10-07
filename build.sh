1d553623420ef799d37679860ee13ad9ba16b6d3
paulgilchrist <PGilchrist@pulte.com>
2022-10-07 09:39:22 -0400

    Use swagger-ui distribution

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ docker push paulgilchrist/swagger-ui:arm64v8
 echo "$(tput setaf 2)"
 echo "Building paulgilchrist/swagger-ui:arm64v8"
 echo "$(tput setaf 7)"
-docker build --rm -f "Dockerfile" --no-cache --platform linux/amd64 -t paulgilchrist/swagger-ui:arm64v8 .
+docker build --rm -f "Dockerfile" --no-cache --platform linux/amd64 -t paulgilchrist/swagger-ui:amd64 .
 echo "$(tput setaf 2)"
 echo "Pushing paulgilchrist/swagger-ui:arm64v8 to https://hub.docker.com"
 echo "$(tput setaf 7)"
-docker push paulgilchrist/swagger-ui:arm64v8
+docker push paulgilchrist/swagger-ui:amd64
 echo "$(tput setaf 2)"
 echo "Removing paulgilchrist/swagger-ui:latest manifest"
 echo "$(tput setaf 7)"
